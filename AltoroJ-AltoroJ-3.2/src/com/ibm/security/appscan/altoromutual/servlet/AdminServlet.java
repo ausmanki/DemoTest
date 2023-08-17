e790fe99d0862b40654a8ac217d8e5dd7ecbbfd5
ausmanki <100470886+ausmanki@users.noreply.github.com>
2023-08-17 09:35:50 +0530

    Update AdminServlet.java

--- AltoroJ-AltoroJ-3.2/src/com/ibm/security/appscan/altoromutual/servlet/AdminServlet.java
+++ AltoroJ-AltoroJ-3.2/src/com/ibm/security/appscan/altoromutual/servlet/AdminServlet.java
@@ -1,6 +1,6 @@
 /**
  This application is for demonstration use only. It contains known application security
-vulnerabilities that were created expressly for demonstrating the functionality of
+vulnerabilities that were created expressly for demonstrating the functionality of 
 application security testing tools. These vulnerabilities may present risks to the
 technical environment in which the application is installed. You must delete and
 uninstall this demonstration application upon completion of the demonstration for
